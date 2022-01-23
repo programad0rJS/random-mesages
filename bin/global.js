e006677273a2b3d284ab68748de08a9371941b61
josue <83614619+josuefeo123@users.noreply.github.com>
2022-01-23 17:46:34 -0500

    random-mesages
    random-mesages

---
+++ bin/global.js
@@ -0,0 +1,7 @@
+// se va ejecutar dentro de bash
+
+//Variable que llama la funcion que exportamos
+let random = require('../src/index.js')
+
+//Ejecuto la funcion
+random.randomMsg()
---
+++ index.js
@@ -0,0 +1,20 @@
+// Se declara el arreglo
+const messages = [
+    "David",
+    "Diana",
+    "Ana Maria",
+    "Isabela",
+    "Antonio",
+    "Norma"
+];
+
+
+
+//Crear función para enviar aleatoriamente  los nombres del arreglo
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random()*messages.length)];
+    console.log(message);
+};
+
+
+module.exports = { randomMsg }
